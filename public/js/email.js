4d67d188e6cc5249b418a0d599bf041482440d42
Richard Schaefer <rchrdschfr@gmail.com>
2017-06-18 14:16:42 +0800

    saving

---
+++ public/js/email.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('send-button').addEventListener('click', function() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'send-email');
+    var postBody = JSON.stringify({
+      name: document.getElementById('form1').value,
+      from: document.getElementById('form2').value,
+      message: document.getElementById('form3').value,
+    });
+    xhr.onreadystatechange = function () {
+      if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        document.getElementById('form1').value = '';
+        document.getElementById('form2').value = '';
+        document.getElementById('form3').value = '';
+      }
+    };
+    xhr.send(postBody)
+  });
+});
