a8e1fb773f0d16469482765b4fbbeb0a9d81db03
Richard Schaefer <rchrdschfr@gmail.com>
2017-06-18 14:30:54 +0800

    saving

--- index.js
+++ index.js
@@ -11,12 +11,11 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')));
 app.post('/send-email', function(req, res) {
-  console.log(req.body)
   const { from, name, message } = req.body
 
   const fromEmail = new sendGridHelper.Email(from)
   const toEmail = new sendGridHelper.Email(TO_EMAIL)
-  const subject = `New message from ${name}`
+  const subject = `New message on neerhofcarpentry.com from ${name}`
   const content = new sendGridHelper.Content('text/plain', message)
   const mail = new sendGridHelper.Mail(fromEmail, subject, toEmail, content)
 
@@ -24,10 +23,10 @@ app.post('/send-email', function(req, res) {
     method: 'POST', path: '/v3/mail/send', body: mail.toJSON()
   }), function (error, response) {
     if (error) {
-      console.log('Error response received')
+      res.status(400).send();
+    } else {
+      res.status(200).send();
     }
-    console.log(response);
-    res.status(200).send();
   })
 })
 
