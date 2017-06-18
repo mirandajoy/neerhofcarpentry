17703559c4653dab30de2df2114e9684069ecf0d
Richard Schaefer <rchrdschfr@gmail.com>
2017-06-18 15:38:21 +0800

    saving

--- public/js/testimonials.js
+++ public/js/testimonials.js
@@ -1,9 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
   var currentTestimonial = 1;
+  var timeout;
+  resetTimeout();
 
-  setTimeout(function() {
+  function goToNextTestimonial() {
     document.getElementById('testimonial-' + currentTestimonial).style.display = 'none';
     document.getElementById('testimonial-' + currentTestimonial + 1).style.display = 'block';
     currentTestimonial++;
-  }, 20000);
+  }
+
+  function goToPreviousTestimonial() {
+    document.getElementById('testimonial-' + currentTestimonial).style.display = 'none';
+    document.getElementById('testimonial-' + currentTestimonial - 1).style.display = 'block';
+    currentTestimonial--;
+    timeout = setTimeout
+  }
+
+  function resetTimeout() {
+    timeout = setTimeout(goToNextTestimonial, 20000);
+  }
+
+  document.getElementById('testimonial-next').addEventListener('click', function() {
+    goToNextTestimonial();
+    resetTimeout();
+  });
+  document.getElementById('testimonial-prev').addEventListener('click', function() {
+    goToPreviousTestimonial();
+    resetTimeout();
+  });
 });
